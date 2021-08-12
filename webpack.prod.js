7a670be2c0bd112d5834957c58a6ff372e1ce89e
keithradford <kradford@change.org>
2021-08-11 17:43:49 -0700

    refactor: webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,11 @@
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "production"
+  mode: 'production',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
 });
