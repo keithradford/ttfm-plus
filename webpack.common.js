c9e91ab7db48aa514688a3e840364407ed89a53c
mattbardal <mattbardal@gmail.com>
2022-08-28 11:57:38 -0600

    clean up eventPage

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,6 @@ const path = require("path");
 module.exports = {
   entry: {
     popup: path.join(__dirname, "src/popup/index.tsx"),
-    eventPage: path.join(__dirname, "src/eventPage.ts"),
     emotes: path.join(__dirname, 'src/emotes.ts'),
     autolike: path.join(__dirname, 'src/autolike.ts'),
     darktheme: path.join(__dirname, 'src/darktheme.ts')
